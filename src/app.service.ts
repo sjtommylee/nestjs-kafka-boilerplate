d47d66ab023b2792679a9ba6d75be2611b16801e
sjtommylee <sjtommylee@gmail.com>
2023-02-11 13:51:37 -0800

    wip

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ProducerService } from './modules/kafka/producer.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly producerService: ProducerService) {}
+  async getHello() {
+    await this.producerService.produce({
+      topic: 'test',
+      messages: [
+        {
+          value: 'hello world',
+        },
+      ],
+    });
+    return 'hello world';
   }
 }
--- src/modules/kafka/consumer.service.ts
+++ src/modules/kafka/consumer.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { Consumer, Kafka } from 'kafkajs';
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
+import {
+  Consumer,
+  ConsumerRunConfig,
+  ConsumerSubscribeTopics,
+  Kafka,
+} from 'kafkajs';
 
 @Injectable()
-export class ConsumerService {
+export class ConsumerService implements OnApplicationShutdown {
   private readonly kafka: Kafka;
-  private readonly consumers: Consumer[] = [];
+  private readonly consumers: Consumer[] = []; // we can have an array of consumers to a producer.
+
+  async consume(topic: ConsumerSubscribeTopics, config: ConsumerRunConfig) {
+    const consumer = this.kafka.consumer({ groupId: 'nestjs-kafka' });
+    await consumer.connect();
+    await consumer.subscribe(topic);
+    await consumer.run(config);
+    this.consumers.push(consumer);
+  }
+
+  async onApplicationShutdown(signal?: string) {
+    for (const consumer of this.consumers) {
+      await consumer.disconnect();
+    }
+  }
 }
