b123eed1d0225e30c43320aee8e1902bee8af961
sjtommylee <sjtommylee@gmail.com>
2023-03-02 01:41:44 -0800

    switching over to microservices

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'service-name-clientid',
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'groupid-in-consumer',
+        },
+      },
+    },
+  );
+  Logger.log('service listening from main.ts');
+  await app.listen();
 }
 bootstrap();
